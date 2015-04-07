8cd6b45482ff3d13999323fb4a0bdc5298a6adcd
Stefan Toman <toman@tum.de>
2015-04-07 17:53:26 +0200

    remove updates of last_login and last_ip_address since this data is now read-only

--- lib/www/auth.php
+++ lib/www/auth.php
@@ -380,8 +380,9 @@ function do_login()
 
 	// Authentication success. We could just return here, but we do a
 	// redirect to clear the POST data from the browser.
-	$DB->q('UPDATE user SET last_login = %s, last_ip_address = %s
-	        WHERE username = %s', now(), $ip, $username);
+	// do not update last_login and last_ip_address since this data is replicated
+	// $DB->q('UPDATE user SET last_login = %s, last_ip_address = %s
+	//        WHERE username = %s', now(), $ip, $username);
 	$script = ($_SERVER['PHP_SELF']);
 	if ( preg_match( '/\/public\/login\.php$/', $_SERVER['PHP_SELF'] ) ) {
 		logged_in(); // fill userdata
