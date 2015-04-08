00fbcf738c961ac8b2b692fe09cb96d589362f59
Stefan Toman <toman@tum.de>
2015-04-08 17:14:56 +0200

    add icons for team and public interface

--- www/team/menu.php
+++ www/team/menu.php
@@ -1,15 +1,15 @@
 <ul class="nav navbar-nav">
 <?php
-echo "<li><a target=\"_top\" href=\"index.php\" accesskey=\"o\">overview</a></li>\n";
+echo '<li><a href="index.php" class="dj-tooltip" data-toggle="tooltip" data-placement="bottom" title="overview"><span class="glyphicon glyphicon-home"></span></a></li>'."\n";
 
 if ( have_problemtexts() ) {
-	echo "<li><a target=\"_top\" href=\"problems.php\" accesskey=\"t\">problems</a></li>\n";
+	echo "<li><a href="problems.php" class="dj-tooltip" data-toggle="tooltip" data-placement="bottom" title="problems"><span class="glyphicon glyphicon-file"></span></a></li>'."\n";
 }
 
 if ( have_printing() ) {
-	echo "<li><a target=\"_top\" href=\"print.php\" accesskey=\"p\">print</a></li>\n";
+	echo "<li><a href="print.php" class="dj-tooltip" data-toggle="tooltip" data-placement="bottom" title="print"><span class="glyphicon glyphicon-print"></span></a></li>'."\n";
 }
-echo "<li><a target=\"_top\" href=\"scoreboard.php\" accesskey=\"b\">scoreboard</a></li>\n";
+echo "<li><a href="scoreboard.php" class="dj-tooltip" data-toggle="tooltip" data-placement="bottom" title="scoreboard"><span class="glyphicon glyphicon-th-list"></span></a></li>'."\n";
 
 if ( checkrole('jury') || checkrole('balloon') ) {
 	echo "<li><a target=\"_top\" href=\"../jury/\" accesskey=\"j\">→jury</a></li>\n";
