f4ee6497afc917efb3c0c433b78941845e5f998f
Stefan Toman <git@stoman.de>
2015-04-22 11:41:55 +0200

    fix some typos

--- www/public/changelog.php
+++ www/public/changelog.php
@@ -17,7 +17,7 @@ D-85748 Garching
 
 <h2>Changelog</h2>
 
-<h3>v.4.1.0.2</h3>
+<h3>v4.1.0.2</h3>
 <ul>
 <li>categrorize problems with topics, authors, difficulty and source</li>
 <li>statistics on registration page</li>
@@ -28,14 +28,14 @@ D-85748 Garching
 <li>fix: size of login input</li>
 </ul>
 
-<h3>v.4.1.0.1</h3>
+<h3>v4.1.0.1</h3>
 <ul>
 <li>fix: action of login forms</li>
 <li>fix: javascript errors removed</li>
 <li>fix: tooltips on all pages</li>
 </ul>
 
-<h3>v.4.1.0.0</h3>
+<h3>v4.1.0.0</h3>
 <ul>
 <li>new layout</li>
 <li>multi server setup with replicated user and team tables</li>
