2977573eef206717cd8267c75714934dd51b4def
Stefan Toman <git@stoman.de>
2015-04-30 11:48:10 +0200

    bugfixes

--- www/jury/team.php
+++ www/jury/team.php
@@ -281,7 +281,7 @@ echo "</h3>\n\n";
 $restrictions['teamid'] = $id;
 putSubmissions($cdatas, $restrictions);
 
-ech '<h3>Points Overview</h3>';
+echo '<h3>Points Overview</h3>';
 putPointsOverview($id);
 
 require(LIBWWWDIR . '/footer.php');
--- www/team/points.php
+++ www/team/points.php
@@ -4,7 +4,6 @@ require('init.php');
 $title = 'Points Overview';
 
 require(LIBWWWDIR . '/header.php');
-require(LIBWWWDIR . '/scoreboard.php');
 
 echo "<h1>" . $title . "</h1>\n\n";
 putPointsOverview($teamdata['teamid']);
