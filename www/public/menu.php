217aa0f8c528ef647dd5916bfc555fee60531f20
Stefan Toman <git@stoman.de>
2015-08-28 14:51:25 +0200

    remove login link form public menu

--- www/public/menu.php
+++ www/public/menu.php
@@ -1,7 +1,7 @@
 <li><a href="index.php"><span class="glyphicon glyphicon-home"></span> home</a></li>
-<?php if(!logged_in()) { ?>
+<?php /*if(!logged_in()) { ?>
 	<li><a href="login.php"><span class="glyphicon glyphicon-user"></span> login</a></li>
-<?php } ?>
+<?php }*/ ?>
 <li><a href="scoreboard.php"><span class="glyphicon glyphicon-th-list"></span> scoreboard</a></li>
 
 <?php
