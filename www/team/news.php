f8561860368ded2b47ee7cf4d7e26e44f5188274
Moritz Fuchs <fuchsmo@in.tum.de>
2015-11-06 13:32:27 +0100

    Set cookie

--- www/team/news.php
+++ www/team/news.php
@@ -3,7 +3,11 @@
 require('init.php');
 require(LIBWWWDIR . '/header.php');
 
+
 foreach(json_decode(file_get_contents('https://judge.in.tum.de/news/latest')) AS $news) {
+  /*Remember the last time we watched the news, used to disply "new" label if 
+  there are new news. */
+  setcookie("lastNewsVisit", time(), time()+1000*24*60*60);
 ?>
 <div class="img-rounded" style="height: 300px; width: 100%; background-color: #002143; background-image: url(<?php echo $news->image_url; ?>); background-position: center; margin: 2em 0; position: relative;">
   <div class="container">
