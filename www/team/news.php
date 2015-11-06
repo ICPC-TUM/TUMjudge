7f6f94edab5a0285dbea5f2e847086d40b99cfa8
Stefan Toman <git@stoman.de>
2015-11-06 13:04:42 +0100

    add url of news images

--- www/team/news.php
+++ www/team/news.php
@@ -5,7 +5,7 @@ require(LIBWWWDIR . '/header.php');
 
 foreach(json_decode(file_get_contents('https://judge.in.tum.de/news/latest.json')) AS $news) {
 ?>
-<div class="img-rounded" style="height: 300px; width: 100%; background-color: #002143; background-image: url(/files/news_images/<?php echo 'todo'; ?>); background-position: center; margin: 2em 0; position: relative;">
+<div class="img-rounded" style="height: 300px; width: 100%; background-color: #002143; background-image: url(<?php echo $news->image_url; ?>); background-position: center; margin: 2em 0; position: relative;">
   <div class="container">
     <div class="carousel-caption">
       <h1 style="margin-bottom: 30px; text-shadow: 0 0 4px black;"><?php echo $news->title; ?></h1>
