fcb38137e9e5b7c1a8ca00b6ee2ed6c0c1f4d476
Moritz Fuchs <fuchsmo@in.tum.de>
2015-12-21 14:20:09 +0100

    error reporting

--- www/team/show_source.php
+++ www/team/show_source.php
@@ -1,11 +1,11 @@
 <?php
 
-require('init.php');
-require(LIBDIR . '/lib.sourcecode.php');
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+require('init.php');
+require(LIBDIR . '/lib.sourcecode.php');
 
 $id = getRequestID();
 
