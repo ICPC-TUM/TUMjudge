c5011e584ebd24ef3f8828728858e956d5bf075c
Moritz Fuchs <fuchsmo@in.tum.de>
2015-12-21 11:04:29 +0100

    debug

--- www/team/submission_download.php
+++ www/team/submission_download.php
@@ -4,7 +4,7 @@ require('init.php');
 $id = getRequestID();
 
 // select also on teamid so we can only select our own submissions
-$row = $DB->q('MAYBETUPLE SELECT s.valid,sf.sourcecode,sf.filename, 
+$row = $DB->q('SELECT s.valid,sf.sourcecode,sf.filename 
 	       FROM submission s
                LEFT JOIN submission_file sf ON (sf.submitid = s.submitid)
                WHERE s.submitid = %i AND s.teamid = %i',$id,$teamid);
