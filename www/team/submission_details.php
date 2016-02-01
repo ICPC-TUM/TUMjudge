caa0b58ad6fad42d43ed2c8040aaf622baf57480
Moritz Fuchs <fuchsmo@in.tum.de>
2016-02-01 13:34:01 +0100

    Add exit

--- www/team/submission_details.php
+++ www/team/submission_details.php
@@ -33,6 +33,7 @@ if( !$row ||
 if($row['submittime'] >= $cdata['endtime']) {
 	header("HTTP/1.1 302 Found"); 
 	header("location: ./index.php");
+	exit();
 }
 
 // update seen status when viewing submission
