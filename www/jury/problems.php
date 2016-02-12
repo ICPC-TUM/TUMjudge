7592a7bce1a24ee228d21c165d2d063fe1a8d4e9
Moritz Fuchs <fuchsmo@in.tum.de>
2016-02-12 11:36:22 +0100

    remove tokenfield, use tokeninput

--- www/jury/problems.php
+++ www/jury/problems.php
@@ -34,10 +34,20 @@ if ( count($cids)!=0 ) {
 
 echo "<script type='text/javascript'>
 $(function() {
-	var topics = [
-	  {value:'BFS/DFS', label:'DFS/BFS'}
-	  ];
-	$('#topics_filter').tokenfield('setTokens', topics);
+	$(\"#topics_filter\").tokenInput([
+                {id: 7, name: \"Ruby\"},
+                {id: 11, name: \"Python\"},
+                {id: 13, name: \"JavaScript\"},
+                {id: 17, name: \"ActionScript\"},
+                {id: 19, name: \"Scheme\"},
+                {id: 23, name: \"Lisp\"},
+                {id: 29, name: \"C#\"},
+                {id: 31, name: \"Fortran\"},
+                {id: 37, name: \"Visual Basic\"},
+                {id: 41, name: \"C\"},
+                {id: 43, name: \"C++\"},
+                {id: 47, name: \"Java\"}
+            ]);
 });
 </script>";
 
