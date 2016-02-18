a427bd52f739463372a0eb5a31ce1465b54e145e
Moritz Fuchs <fuchsmo@in.tum.de>
2016-02-18 14:14:24 +0100

    Play with margins, remove container

--- lib/lib.sourcecode.php
+++ lib/lib.sourcecode.php
@@ -253,7 +253,7 @@ function presentSourceCode($id, $teamid=-1) {
 	$html .= '<div class="tabbertab">' .
 		'<div id="rrcContainer">' .
 		'<h2 class="filename">Run Random Case</h2>' . 
-		'<div id="runrandomcaseContainer"><button onClick="javascript:sendSubmission()">Search for a failing testcase</button>' .
+		'<button onClick="javascript:sendSubmission()">Search for a failing testcase</button>' .
 		'<button onClick="javascript:toggleLog();" id="rrcLogButton">Hide Log</button><br /><br />'.
 		'<input type="hidden" value="' . $problemName . '" name="problemName" id="rrcProblemName">' .
 		'<input type="hidden" value="' . $submission['langid'] . '" name="submissionLanguage" id="rrcSubmissionLanguage">' .
@@ -262,7 +262,6 @@ function presentSourceCode($id, $teamid=-1) {
 		'<div id="rrcLog"></div>'.
 		'</div>'.
 		'<div id="rrcResult"></div>'.
-		'</div>' .
 		'</div>' .
 		'</div>';
 
