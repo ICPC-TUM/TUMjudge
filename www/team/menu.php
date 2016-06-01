fda428ed05dcf46ec95f9c91cb5d066551203562
Stefan Toman <git@stoman.de>
2016-06-01 13:13:51 +0200

    add docs to menu

--- www/team/menu.php
+++ www/team/menu.php
@@ -2,7 +2,7 @@
 <li><a href="problems.php"><span class="glyphicon glyphicon-flag"></span> problems</a></li>
 <li><a href="scoreboard.php"><span class="glyphicon glyphicon-th-list"></span> scoreboard</a></li>
 <li><a href="help.php"><span class="glyphicon glyphicon-question-sign"></span> help</a></li>
-<li><a href="https://judge.in.tum.de/docs/" target="_blank"><span class="glyphicon glyphicon-book"></span> help</a></li>
+<li><a href="https://judge.in.tum.de/docs/" target="_blank"><span class="glyphicon glyphicon-book"></span> docs</a></li>
 
 <?php
 putClock();
