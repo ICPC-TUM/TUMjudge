73b5e19df76047f11b2b13ad5be3ccc492e8d980
Stefan Toman <git@stoman.de>
2017-05-09 17:26:54 +0200

    move gcpc judge to new path, change default css class

--- lib/www/header.php
+++ lib/www/header.php
@@ -67,7 +67,7 @@ if ( ! empty($extrahead) ) echo $extrahead;
 </head>
 <?php
 
-$cssclass = (!empty($cid) ? 'contest-'.$cid.' ' : '').'tumjudge-'.$_GET['tumjudge_instance'];
+$cssclass = (!empty($cid) ? 'contest-'.$cid.' ' : '').'tumjudge-'.(array_key_exists('tumjudge_instance', $_GET) ? $_GET['tumjudge_instance'] : 'gcpc');
 if ( IS_JURY ) {
 	global $pagename;
 	echo "<body class=\"".$cssclass."\" onload=\"setInterval('updateMenu(" .
@@ -113,6 +113,7 @@ if ( IS_JURY ) {
             <li><a class="pagetitle">ICPC@TUM</a></li>
             <?php if(IS_JURY): ?>
               <li class="page tumjudge-git"><a href="https://git-judge.in.tum.de">GIT <span class="description">Source Code Management</span></a></li>
+              <li class="page tumjudge-git"><a href="https://chat-judge.in.tum.de">Chat <span class="description">Mattermost Chat</span></a></li>
             <?php endif; ?>
             <li class="page tumjudge-info"><a href="https://icpc.tum.de">Website <span class="description">Information about the ICPC</span></a></li>
             <li class="page tumjudge-discuss"><a href="https://judge.in.tum.de/discuss/">Discuss <span class="description">Discuss all about TUMjudge and ICPC</span></a></li>
@@ -125,7 +126,7 @@ if ( IS_JURY ) {
             <li class="page tumjudge-contest"><a href="https://judge.in.tum.de/contest/">Contest <span class="description">Preparation for the ACM ICPC</span></a></li>
             <li class="page tumjudge-gad"><a href="https://judge.in.tum.de/gad/">GAD <span class="description">Lecture &ldquo;Foundations: Algorithms and Data Structures&rdquo;</span></a></li>
             <li class="page tumjudge-isabelle"><a href="https://judge.in.tum.de/isabelle/">Isabelle <span class="description">Proving Contests</span></a></li>
-            <li class="page tumjudge-gcpc"><a href="https://judge.in.tum.de/gcpc/">GCPC <span class="description">German Collegiate Programming Contest</span></a></li>
+            <li class="page tumjudge-gcpc"><a href="https://judge.in.tum.de/judge/">GCPC <span class="description">German Collegiate Programming Contest</span></a></li>
             <li class="page tumjudge-ioide"><a href="https://judge.in.tum.de/ioide/">IOI Germany <span class="description">Preparation for the IOI</span></a></li>
             <li class="page tumjudge-ioiat"><a href="https://judge.in.tum.de/ioiat/">IOI Austria <span class="description">Preparation for the IOI</span></a></li>
             <li class="page tumjudge-challenge"><a href="https://judge.in.tum.de/challenge/">Challenge <span class="description">Yet Another Programming Contest</span></a></li>
